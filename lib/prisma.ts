d65397c13d47b6929748cd57e4adf639027c16ff
ricoadtxx <euricoadit555@gmail.com>
2024-12-26 16:55:18 +0700

    adding neon db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,26 @@
+/* eslint-disable no-var */
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 
-const prismaClientSingleton = () => {
-	return new PrismaClient();
-};
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
 
-declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+neonConfig.poolQueryViaFetch = true;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// Type definitions
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
