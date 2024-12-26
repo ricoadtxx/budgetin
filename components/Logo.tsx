b739ca99ad73cc3c07d59a4b5df68504649eb5f2
ricoadtxx <euricoadit555@gmail.com>
2024-12-26 20:25:47 +0700

    adding logo paw

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,11 @@
+import { PawPrint } from "lucide-react";
 import Link from "next/link";
-import { PiggyBank } from "lucide-react";
 import React from "react";
 
 function Logo() {
 	return (
 		<Link href="/" className="flex items-center gap-1">
-			<PiggyBank className="stroke h-11 w-11 stroke-red-300 stroke-[1.5]" />
+			<PawPrint className="stroke h-11 w-11 stroke-red-300 stroke-[1.5]" />
 			<p className="bg-gradient-to-r from-red-200 to-red-300 bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent">
 				Budget Tracker
 			</p>
