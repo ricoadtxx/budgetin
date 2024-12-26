434f936235a4425cd682f1f0b5ab32fc7bbcbad3
ricoadtxx <euricoadit555@gmail.com>
2024-12-26 20:27:38 +0700

    adding logo paw

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function Logo() {
 	return (
 		<Link href="/" className="flex items-center gap-1">
-			<PawPrint className="stroke h-11 w-11 stroke-red-300 stroke-[1.5]" />
+			<PawPrint className="stroke h-8 w-8 stroke-red-300 stroke-[1.5]" />
 			<p className="bg-gradient-to-r from-red-200 to-red-300 bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent">
 				Budget Tracker
 			</p>
